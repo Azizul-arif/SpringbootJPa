62b03f43cc6b616abe891dfb7bb9d190766ca094
Azizul-arif <azizularif94@gmail.com>
2026-03-06 22:03:38 +0600

    (add):Error Exception

---
+++ src/main/java/com/propertyManagement/propertyManagement/exception/ErrorModel.java
@@ -0,0 +1,11 @@
+package com.propertyManagement.propertyManagement.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorModel {
+    private String code;
+    private String message;
+}
